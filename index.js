5086a17b08832d11f5b3e9e073add417761b415f
Nicholas Cable <cab17001@byui.edu>
2021-03-12 22:54:49 -0700

    created rooms, allow messages between ppl

--- index.js
+++ index.js
@@ -15,11 +15,37 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html');
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html');
+});
+
+// creating tech namespace
+const tech = io.of('/tech');
+
 // whenever a connection is made || something happens
-io.on('connection', (socket) => {
+tech.on('connection', (socket) => {
     console.log('User connected');
-    socket.on('message', (msg) => {
-        console.log(`message: ${msg}`);
-        io.emit('message', msg);
+
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New User joined ${data.room} room!`);
     });
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        tech.emit('message', 'user disconnected');
+    })
 });
